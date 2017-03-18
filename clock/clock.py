b4e85367297096b10bbe80ee04cdfc8faee20436
Matheus Bittencourt <matheuspb2005@hotmail.com>
2017-03-18 18:57:26 -0300

    Improvements in Clock exercise

--- clock/clock.py
+++ clock/clock.py
@@ -1,17 +1,18 @@
 class Clock:
 
     def __init__(self, hours, minutes):
-        self.__minutes = minutes + (60 * hours)
+        self.__minutes = 0
+        self.add(hours * 60 + minutes)
 
     def __str__(self):
-        hours = (self.__minutes // 60) % 24
-        return "{:02d}:{:02d}".format(hours, self.__minutes % 60)
+        hours, minutes = divmod(self.__minutes, 60)
+        return "{:02d}:{:02d}".format(hours, minutes)
 
     __repr__ = __str__
 
     def __eq__(self, other):
-        return self.__minutes % 1440 == other.__minutes % 1440
+        return self.__minutes == other.__minutes
 
     def add(self, minutes):
-        self.__minutes += minutes
+        self.__minutes = (self.__minutes + minutes) % 1440
         return self
